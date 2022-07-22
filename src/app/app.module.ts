35928c4890b16f313320f1c9a1fe53c67c39854e
Lucas <lucasdebruchard@gmail.com>
2022-07-21 23:04:50 -0300

    ecommerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/shared/footer/footer.component';
+import { HeaderComponent } from './component/shared/header/header.component';
+import { NavComponent } from './component/shared/nav/nav.component';
+import { BookstoreAppComponent } from './component/bookstore-app/bookstore-app.component';
+import { FiltersComponent } from './component/bookstore-app/filters/filters.component';
+import { ProductListComponent } from './component/bookstore-app/product-list/product-list.component';
+import { ProductItemComponent } from './component/bookstore-app/product-list/product-item/product-item.component';
+import { BookService } from './component/bookstore-app/product-list/product-list.component.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    BookstoreAppComponent,
+    FiltersComponent,
+    ProductListComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BookService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/component/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+
+import { Book } from "./Model/book";
+
+@Injectable()
+
+export class BookService
+{
+    private url = 'https://localhost:44382/api/bookstore';
+
+    httpOptions = {
+        Headers: new HttpHeaders({'content-type' : 'application/json'})
+    }
+
+    constructor(private http:HttpClient){}
+
+    getBook(){
+        return this.http.get(this.url)
+    }
+}
